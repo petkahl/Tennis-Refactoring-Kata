05e43a3fb4ae8fdce075b29a3f66050d8577f64c
Peter Kahle <petkahl@microsoft.com>
2017-03-09 21:34:48 -0700

    Trying to reduce complexity.

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,5 +1,16 @@
+using System;
+
 namespace Tennis
 {
+
+    enum Score
+    {
+        Love = 0,
+        Fifteen = 1,
+        Thirty = 2,
+        Forty = 3
+    }
+
     class TennisGame1 : ITennisGame
     {
         private int m_score1 = 0;
@@ -24,66 +35,71 @@ namespace Tennis
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                Player1WonPoint();
             else
-                m_score2 += 1;
+                Player2WonPoint();
         }
 
         public string GetScore()
         {
             string score = "";
-            var tempScore = 0;
             if (m_score1 == m_score2)
             {
-                switch (m_score1)
+                if (m_score1 >= 3)
                 {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
+                    score = "Deuce";
+                }
+                else
+                {
+                    score = CalculateOneScore(m_score1) + "-All";
                 }
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
+                string player = "player1";
                 var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                if (minusResult < 0) player = "player2";
+                if (Math.Abs(minusResult) == 1)
+                {
+                    score = "Advantage " + player;
+                } else
+                {
+                    score = "Win for " + player;
+                }
+                //if (minusResult == 1) score = "Advantage player1";
+                //else if (minusResult == -1) score = "Advantage player2";
+                //else if (minusResult >= 2) score = "Win for player1";
+                //else score = "Win for player2";
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                    score = CalculateOneScore(m_score1) + "-" + CalculateOneScore(m_score2);
             }
             return score;
+        }
+
+        private static string CalculateOneScore(int score)
+        {
+            string result = string.Empty;
+            Score s = (Score)score;
+            result = s.ToString();
+            //switch (score)
+            //{
+            //    case 0:
+            //        result = "Love";
+            //        break;
+            //    case 1:
+            //        result = "Fifteen";
+            //        break;
+            //    case 2:
+            //        result = "Thirty";
+            //        break;
+            //    case 3:
+            //        result = "Forty";
+            //        break;
+            //}
+
+            return result;
         }
     }
 }
