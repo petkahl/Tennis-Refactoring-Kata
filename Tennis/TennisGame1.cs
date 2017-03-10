09da44a7e925c1e96cee278c43db6c5355b2f755
Peter Kahle <petkahl@microsoft.com>
2017-03-09 17:22:35 -0700

    Using ScoreToWords in the All case.

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -54,28 +54,31 @@ namespace Tennis
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                score = ScoreToWords(m_score1) + "-" + ScoreToWords(m_score2);
             }
             return score;
+        }
+
+        private string ScoreToWords(int score)
+        {
+            string result = string.Empty;
+            switch (score)
+            {
+                case 0:
+                    result = "Love";
+                    break;
+                case 1:
+                    result = "Fifteen";
+                    break;
+                case 2:
+                    result = "Thirty";
+                    break;
+                case 3:
+                    result = "Forty";
+                    break;
+            }
+
+            return result;
         }
     }
 }
